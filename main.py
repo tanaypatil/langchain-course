0d07a32e91735522d2b44b1b53c2c3a6dcf39120
tanaypatil <tanaypatil197@gmail.com>
2025-08-30 20:54:48 +0530

    Added output parser

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import create_react_agent, AgentExecutor
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(temperature=0, model="gpt-4")
+
 # prompt template for ReAct from hub
 react_prompt = hub.pull("hwchase17/react")
+
+# output parser
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+# reAct prompt
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
 # creates chain
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt
+    prompt=react_prompt_with_format_instructions
 )
 # executes the tools as per reasoning by llm
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
